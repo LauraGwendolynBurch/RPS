83695a899c9e1cb8d90695377d2e23f1e227dab9
Laura Gwen <lauraburch.lb@gmail.com>
2021-07-07 15:14:23 -0700

    added the final variables needed to call the score and text elements.  started the logic to handle the game if there is a tie.  now the text will change but the score will not

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,12 @@ const randomClasses = [
 const butttons = document.querySelectorAll(".selection button");
 const showIcon = document.querySelector(".show i");
 const computerShowIcon = document.querySelector(".computer i");
+const pScore = document.getElementById("playerScore");
+const cScore = document.getElementById("computerScore");
+const text = document.getElementById("demo");
+const text2 = document.getElementById("demo2");
+let computerScore = 1;
+let playerScore = 1;
 
 function round() {
   butttons.forEach((btn) => {
@@ -17,6 +23,15 @@ function round() {
 
       let randomNum = Math.floor(Math.random() * randomClasses.length);
       computerShowIcon.className = randomClasses[randomNum];
+
+      if (showIcon.className === computerShowIcon.className) {
+        pScore.innerHTML = pScore.innerHTML;
+        cScore.innerHTML = cScore.innerHTML;
+        text.innerHTML = "It's a Tie ! ";
+        text.style.color = "crimson";
+        text2.innerHTML = text.innerHTML;
+        text2.style.color = "crimson";
+      }
     });
   });
 }
