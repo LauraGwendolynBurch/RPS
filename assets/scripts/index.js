5c9a513a5397da2fa50a021d8e19fc9b4ac9ff29
Laura Gwen <lauraburch.lb@gmail.com>
2021-07-07 13:36:34 -0700

    added variable to target the computer icon.  Then added a method to randomly select from the randomClasses array, and change the icon of the computer choice.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,13 +6,17 @@ const randomClasses = [
 ];
 const butttons = document.querySelectorAll(".selection button");
 const showIcon = document.querySelector(".show i");
+const computerShowIcon = document.querySelector(".computer i");
 
 function round() {
   butttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-      console.log(e.target);
+      //   console.log(e.target);
       let clickedBtn = e.target.className;
       showIcon.className = clickedBtn;
+
+      let randomNum = Math.floor(Math.random() * randomClasses.length);
+      computerShowIcon.className = randomClasses[randomNum];
     });
   });
 }
